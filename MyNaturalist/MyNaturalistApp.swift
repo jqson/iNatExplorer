9c3a83f0b6dd5384f48fd06211e59c5a7a488fe8
jqson <yuanfengjiao@gmail.com>
2024-12-03 14:25:44 -0800

    Initial commit

---
+++ MyNaturalist/MyNaturalistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNaturalistApp.swift
+//  MyNaturalist
+//
+//  Created by Yuanfeng Jiao on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNaturalistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyNaturalistTests/MyNaturalistTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyNaturalistTests.swift
+//  MyNaturalistTests
+//
+//  Created by Yuanfeng Jiao on 12/3/24.
+//
+
+import Testing
+@testable import MyNaturalist
+
+struct MyNaturalistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
