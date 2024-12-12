09cb9552cd5f2c648f21d877a77662c96c13b0e6
jqson <yuanfengjiao@gmail.com>
2024-12-11 22:20:58 -0800

    Remove GMS so preview can work

--- MyNaturalist/MyNaturalistApp.swift
+++ MyNaturalist/MyNaturalistApp.swift
@@ -6,14 +6,9 @@
 //
 
 import SwiftUI
-import GoogleMaps
 
 @main
 struct MyNaturalistApp: App {
-    
-    init() {
-        GMSServices.provideAPIKey("AIzaSyDQPAmqBULGhHwOj8LPrsHSTn-r-bYfKo4")
-    }
     
     var body: some Scene {
         WindowGroup {
