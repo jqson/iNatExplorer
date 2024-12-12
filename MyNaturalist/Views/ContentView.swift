b12b4da2bc5b58972926801bcdb00da06b138378
jqson <yuanfengjiao@gmail.com>
2024-12-12 00:20:37 -0800

    Init search filter page

--- MyNaturalist/Views/ContentView.swift
+++ MyNaturalist/Views/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var taxonSelected: Bool = false
+    
     var body: some View {
-        ObservationListView()
+        NavigationStack {
+            VStack() {
+                Text("Filter")
+            
+                Text("owl")
+                    .font(.title3)
+                    .cornerRadius(10)
+                    .padding()
+                    .frame(height: 40)
+                    .foregroundStyle(taxonSelected ? Color.white : Color.accentColor)
+                    .background(taxonSelected ? Color.accentColor : Color.white)
+                    .border(Color.accentColor)
+                    .onTapGesture {
+                        taxonSelected.toggle()
+                    }
+                
+                Spacer()
+                
+                NavigationLink(destination: ObservationListView()) {
+                    Text("Search")
+                        .font(.title2)
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(Color.white)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                .navigationTitle("Filter")
+            }
+        }
     }
 }
 
